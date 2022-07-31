3e9ecddadaaa7eedcd089571e9083b7c706c7e8a
Apostolos172 <apostolos.vad@gmail.com>
2022-07-31 12:08:15 +0300

    main function of converter

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+import main.Main_GUI;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("");
-
+		new Main_GUI();
+		
 	}
 
 }
