2e390a611d5992b7628735cb11c52be31fe6d75c
Apostolos172 <apostolos.vad@gmail.com>
2022-09-02 00:03:36 +0300

    some info about the app

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import main.Main_GUI;
 
 public class Main {
-
+	
+	// Calculator App
+	// UTF-8 encoding
+	// Window > Preferences > General > Workspace
 	public static void main(String[] args) {
 		
 		new Main_GUI();
