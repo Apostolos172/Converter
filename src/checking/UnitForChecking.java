285c4732d2716255d7b35253ffc3b01c43fc124c
Apostolos172 <apostolos.vad@gmail.com>
2022-09-08 23:00:14 +0300

    listen for length convertions and separate checking of unit in separate functions and ever further through unitForChecking interface

---
+++ src/checking/UnitForChecking.java
@@ -0,0 +1,6 @@
+package checking;
+
+public interface UnitForChecking {
+	public boolean isIt(String convertionSign, double measurementToConvert, StringBuilder toFieldText);
+	// Να γίνει αυτό για να δούμε το passing
+}
