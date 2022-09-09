04baed13aebd3d3f249ff4c8560153f7be50693a
Apostolos172 <apostolos.vad@gmail.com>
2022-09-10 01:04:43 +0300

    rename beginner class

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import main.Main_GUI;
+
+public class App {
+	
+	// Calculator App
+	// UTF-8 encoding
+	// Window > Preferences > General > Workspace
+	public static void main(String[] args) {
+		
+		new Main_GUI();
+		
+	}
+
+}
