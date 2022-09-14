abb02eb9510368e0a4a1f241a2ea7b86cddea02e
Apostolos172 <apostolos.vad@gmail.com>
2022-09-14 22:46:53 +0300

    add different look and feel to application using external library flatlaf

--- src/App.java
+++ src/App.java
@@ -1,3 +1,10 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import main.Main_GUI;
 
 public class App {
@@ -7,8 +14,21 @@ public class App {
 	// Window > Preferences > General > Workspace
 	public static void main(String[] args) {
 		
+		changeLookAndFeel();		
 		new Main_GUI();
 		
 	}
+
+	private static void changeLookAndFeel() {
+		// TODO Auto-generated method stub
+		try {
+			UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			Logger.getLogger(Main_GUI.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		JFrame.setDefaultLookAndFeelDecorated(true);
+
+	}
+	
 
 }
